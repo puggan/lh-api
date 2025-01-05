3e944d5a891671fb8b793001114d830563ed6cb8
Puggan Sundragon <puggan@sundragon.se>
2025-01-05 08:03:44 +0100

    Replace empty.tsx example with test.tsx

---
+++ public/ts/test.tsx
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tsxScriptElement = document.querySelector<HTMLScriptElement>('script[data-tsx="test"]');
+    const scriptContent = (
+        <div>
+            TEST
+        </div>
+    );
+    if (tsxScriptElement) {
+        tsxScriptElement.parentElement.insertBefore(
+            scriptContent,
+            tsxScriptElement.nextElementSibling
+        );
+    } else {
+        document.body.appendChild(scriptContent);
+    }
+}, {once: true, passive: true});
